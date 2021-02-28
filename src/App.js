5a4721965d18b5662dab162566b6be0b65fb9ab4
prwn90 <purwin.daniel@gmail.com>
2021-02-28 13:08:12 +0100

    new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
-function App() {
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="PROJECT-ID here"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a').play()}
+    />
   );
-}
+};
 
 export default App;
